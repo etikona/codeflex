63ff87e3ac4c8bb332f8e6448d7a68c0311773fd
eti kona <etikonapal@gmail.com>
2025-04-30 12:37:04 +0600

    first commit

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <h3>Sign In Page</h3>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+const SignUpPAge = () => {
+  return (
+    <div>
+      <h3>Sign Up Page</h3>
+    </div>
+  );
+};
+
+export default SignUpPAge;
